f5959b9f7bad1a54563beca04efbe039db60016b
Javontay McElroy <dinomightnetwork@gmail.com>
2019-04-14 23:29:54 -0500

    Pushing to pull.

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const server = express();
 
