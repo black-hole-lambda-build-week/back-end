825170e19dc886b357e7eba33df8194bafae2756
Javontay McElroy <dinomightnetwork@gmail.com>
2019-04-17 11:02:36 -0500

    pushing to master.

--- seeds/messages.js
+++ seeds/messages.js
@@ -1,9 +1,7 @@
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex('messages')
     .del()
     .then(function() {
-      // Inserts seed entries
       return knex('messages').insert([
         {
           id: 1,
